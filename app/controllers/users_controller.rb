9dcdd42e353290026e38e777d4362c15d254f16d
nqngoc198 <nguyenquangngoc1981995@gmail.com>
2018-04-23 13:29:31 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find params[:id]
+    if !logged_in?
+      redirect_to login_path
+    end
+    @user = User.find_by id: params[:id]
   end
 
   def new
